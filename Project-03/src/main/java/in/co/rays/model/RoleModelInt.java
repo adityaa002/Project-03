932dea2f473acc9006a6c1230a347d9c754793cf
adityaa002 <aaeaditya02@gmail.com>
2025-11-05 20:09:29 +0530

    05-11-2025

--- Project-03/src/main/java/in/co/rays/model/RoleModelInt.java
+++ Project-03/src/main/java/in/co/rays/model/RoleModelInt.java
@@ -3,24 +3,25 @@ package in.co.rays.model;
 import java.util.List;
 
 import in.co.rays.dto.RoleDTO;
+import in.co.rays.exception.ApplicationException;
+import in.co.rays.exception.DuplicateRecordException;
 
 public interface RoleModelInt {
-	public long add(RoleDTO dto);
+	public long add(RoleDTO dto) throws ApplicationException, DuplicateRecordException;
 
-	public void update(RoleDTO dto);
+	public void delete(RoleDTO dto) throws ApplicationException;
 
-	public void delete(RoleDTO dto);
+	public void update(RoleDTO dto) throws ApplicationException, DuplicateRecordException;
 
-	public List list(int pageNo, int pageSize);
+	public List list() throws ApplicationException;
 
-	public List list();
+	public List list(int pageNo, int pageSize) throws ApplicationException;
 
-	public List search(RoleDTO dto, int pageNo, int pageSize);
+	public List search(RoleDTO dto) throws ApplicationException;
 
-	public List search(RoleDTO dto);
+	public List search(RoleDTO dto, int pageNo, int pageSize) throws ApplicationException;
 
-	public RoleDTO findByPk(long id);
+	public RoleDTO findByPK(long pk) throws ApplicationException;
 
-	public RoleDTO findByName(String name);
-
+	public RoleDTO findByName(String name) throws ApplicationException;
 }
--- Project-03/src/main/java/in/co/rays/test/TestRoleModel.java
+++ Project-03/src/main/java/in/co/rays/test/TestRoleModel.java
@@ -2,39 +2,93 @@ package in.co.rays.test;
 
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 import in.co.rays.dto.RoleDTO;
-import in.co.rays.model.RoleModelHibImpl;
+import in.co.rays.exception.ApplicationException;
+import in.co.rays.exception.DuplicateRecordException;
+import in.co.rays.model.RoleModelHibImp;
 import in.co.rays.model.RoleModelInt;
 
 public class TestRoleModel {
+	public static RoleModelInt model = new RoleModelHibImp();
 
-	public static RoleModelInt model = new RoleModelHibImpl();
+	// public static RoleModelInt model=new RoleModelJDBCImpl;
+	public static void main(String[] args) throws Exception {
+		// addTest();
+		// deleteTest();
+		// updateTest();
+		// findByPkTest();
+		// findByNameTest();
+		// searchTest();
+		 listTest();
+	}
 
-	public static void TestAdd() {
+	public static void listTest() throws ApplicationException {
+		List list = model.list(0, 0);
+		Iterator it = list.iterator();
+		while (it.hasNext()) {
+			RoleDTO dto = (RoleDTO) it.next();
+			System.out.println(dto.getId() + "\t" + dto.getName() + "\t" + dto.getDescription());
+		}
 
+	}
+
+	public static void searchTest() throws ApplicationException {
+		// TODO Auto-generated method stub
+		RoleDTO dto1 =new RoleDTO();
+		//dto1.setId(1L);
+		dto1.setName("admin");
+		List<RoleDTO> a=(List<RoleDTO>) model.search(dto1, 0, 0);
+		for(RoleDTO dto: a){
+			System.out.println(dto.getId()+"\t"+dto.getName()+"\t"+dto.getDescription());
+		}
+	}
+
+	public static void findByNameTest() throws ApplicationException {
+		RoleDTO dto = model.findByName("Admin");
+		System.out.println(dto.getId() + "\t" + dto.getName() + "\t" + dto.getDescription());
+	}
+
+	public static void updateTest() throws ApplicationException, DuplicateRecordException {
 		RoleDTO dto = new RoleDTO();
-		dto.setName("Rays");
-		dto.setDescription("Student panel");
-		dto.setCreatedBy("aditya@gmail.com");
-		dto.setModifiedBy("aditya@gmail.com");
+		dto.setId(6L);
+		dto.setName("admin");
+		dto.setDescription("Admsdfgdinistrator");
+		dto.setCreatedBy("admin");
+		dto.setModifiedBy("admin");
 		dto.setCreatedDatetime(new Timestamp(new Date().getTime()));
 		dto.setModifiedDatetime(new Timestamp(new Date().getTime()));
-		model.add(dto);
-
-	}
-
-	public static void TestUpdate() {
-		RoleDTO dto = new RoleDTO();
-		dto.setName("Department");
-		dto.setId(6L);
+		System.out.println("Record Updated");
 		model.update(dto);
-
 	}
 
-	public static void main(String[] args) {
-		//TestAdd();
-		TestUpdate();
+	public static void findByPkTest() throws ApplicationException {
+		// TODO Auto-generated method stub
+		RoleDTO dto = model.findByPK(6);
+		System.out.println(dto.getId() + "\t" + dto.getName() + "\t" + dto.getDescription());
 	}
 
+	public static void deleteTest() throws ApplicationException {
+		// TODO Auto-generated method stub
+		RoleDTO dto = new RoleDTO();
+		dto.setId(7L);
+		model.delete(dto);
+	}
+
+	public static void addTest() throws Exception {
+		// TODO Auto-generated method stub
+		RoleDTO dto = new RoleDTO();
+
+		dto.setName("reception");
+		dto.setDescription("Student panel");
+		dto.setCreatedBy("admin@gmail.com");
+		dto.setModifiedBy("admin@gmail.com");
+		dto.setCreatedDatetime(new Timestamp(new Date().getTime()));
+		dto.setModifiedDatetime(new Timestamp(new Date().getTime()));
+		System.out.println("Role Add");
+
+		model.add(dto);
+	}
 }
