5990c5656715e8add7197aedefd2ad6ca1ace8da
adityaa002 <aaeaditya02@gmail.com>
2025-11-06 18:17:59 +0530

    06-11-2025

--- Project-03/src/main/java/in/co/rays/model/UserModelHibImpl.java
+++ Project-03/src/main/java/in/co/rays/model/UserModelHibImpl.java
@@ -1,18 +1,22 @@
 package in.co.rays.model;
 
+import java.util.List;
+
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import in.co.rays.dto.UserDTO;
 import in.co.rays.exception.ApplicationException;
+import in.co.rays.exception.DuplicateRecordException;
+import in.co.rays.exception.RecordNotFoundException;
 import in.co.rays.util.HibDataSource;
 
-public class UserModelHibImpl {
+public class UserModelHibImpl implements UserModelInt {
 	Session session = null;
 	Transaction tx = null;
 
-	public void add(UserDTO dto) throws ApplicationException {
+	public long add(UserDTO dto) throws ApplicationException {
 
 		session = HibDataSource.getSession();
 		tx = session.beginTransaction();
@@ -31,7 +35,79 @@ public class UserModelHibImpl {
 		} finally {
 			tx.rollback();
 		}
+		return pk;
 
+	}
+
+	public void delete(UserDTO dto) throws ApplicationException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void update(UserDTO dto) throws ApplicationException, DuplicateRecordException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public UserDTO findByPK(long pk) throws ApplicationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public UserDTO findByLogin(String login) throws ApplicationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List list() throws ApplicationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List list(int pageNo, int pageSize) throws ApplicationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List search(UserDTO dto, int pageNo, int pageSize) throws ApplicationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List search(UserDTO dto) throws ApplicationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean changePassword(long id, String newPassword, String oldPassword)
+			throws ApplicationException, RecordNotFoundException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public UserDTO authenticate(String login, String password) throws ApplicationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean forgetPassword(String login) throws ApplicationException, RecordNotFoundException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean resetPassword(UserDTO dto) throws ApplicationException, RecordNotFoundException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public long registerUser(UserDTO dto) throws ApplicationException, DuplicateRecordException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public List getRoles(UserDTO dto) throws ApplicationException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- Project-03/src/main/java/in/co/rays/test/TestUserModel.java
+++ Project-03/src/main/java/in/co/rays/test/TestUserModel.java
@@ -1,32 +1,36 @@
 package in.co.rays.test;
 
 import java.sql.Timestamp;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import in.co.rays.dto.UserDTO;
+import in.co.rays.exception.ApplicationException;
+import in.co.rays.exception.DuplicateRecordException;
 import in.co.rays.model.UserModelHibImpl;
 import in.co.rays.model.UserModelInt;
 
 public class TestUserModel {
 
- 
-	public static void main(String[] args) {
+	public static UserModelInt model = new UserModelHibImpl();
+
+	public static void main(String[] args) throws ParseException, ApplicationException, DuplicateRecordException {
 		testAdd();
 
 	}
 
-	private static void testAdd() {
+	private static void testAdd() throws ParseException, ApplicationException, DuplicateRecordException {
 
 		UserDTO dto = new UserDTO();
 		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 
-		dto.setFirstName("Yash");
-		dto.setLastName("Kanoongo");
-		dto.setDob(sdf.parse("13-05-1999"));
-		dto.setConfirmPassword("Yash@12345");
-		dto.setPassword("Yash@12345");
-		dto.setLogin("yashkanoongo493@gmail.com");
+		dto.setFirstName("Aditya");
+		dto.setLastName("Sharma");
+		dto.setDob(sdf.parse("26-07-2002"));
+		dto.setConfirmPassword("adi@12345");
+		dto.setPassword("adi@12345");
+		dto.setLogin("aditya@gmail.com");
 		dto.setGender("male");
 		dto.setUnSuccessfullLogin(2);
 
@@ -34,8 +38,8 @@ public class TestUserModel {
 		dto.setModifiedBy("admin");
 		dto.setRoleId(1);
 		dto.setMobileNo("9406653787");
-		dto.setRegisteredIP("yashkanoongo493@gmail.com");
-		dto.setLoginIP("yashkanoongo493@gmail.com");
+		dto.setRegisteredIP("aditya@gmail.com");
+		dto.setLoginIP("aditya@gmail.com");
 		dto.setLastLogin(new Timestamp(new Date().getTime()));
 		dto.setCreatedDatetime(new Timestamp(new Date().getTime()));
 		dto.setModifiedDatetime(new Timestamp(new Date().getTime()));
